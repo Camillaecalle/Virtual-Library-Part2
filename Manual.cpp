db5b8d1cb3a94b47f3cb0e27989a003d49a2f5ed
Camillaecalle <camilla7732528@gmail.com>
2023-02-21 19:11:08 -0500

    Test

--- Manual.cpp
+++ Manual.cpp
@@ -13,71 +13,47 @@ Manual::Manual(){
     visual_ = false;
     website_ = false;
 
+
 }
 
-/**
-  Parameterized constructor.
-  @param     : The title of the book (a string)
-  @param     : The author of the book (a string)
-  @param     : The number of pages in the book (a positive integer)
-  @param     : The name of the device (a string)
-  @param     : A flag indicating whether the book is in digital form
-                with DEFAULT VALUE False (a Boolean)
-  @param     : The URL that is in the format
-                'https://www.something.something'
-                or 'http://www.something.something'
-                with the last 'something' being at least 2 characters
-                with DEFAULT VALUE empty string (a string)
-  @param     : A Boolean indicating the presence of a visual aid
-                with DEFAULT VALUE False
-  @post      : The private members are set to the values of the
-                corresponding parameters.
-                If a URL is provided, the website flag is set to True.
-                If the URL is ill-formatted, the website is set to
-                empty string and the website flag is set to False.
-*/
 
-Manual::Manual(std::string title, std::string author, int page_count, std::string device, bool is_digital, std::string url, bool visual):Book(title, author, page_count, is_digital){
-    url_=url;
-    device_=device;
-    visual_=visual;
+
+
+
+Manual::Manual(std::string title, std::string author, int page_count, std::string device, bool is_digital, std::string url, bool visual_aid):Book(title, author, page_count, is_digital){
+    
+    device_ = device;
+    visual_ = visual_aid;
+    std::string regexPattern = "https?://www.[a-zA-Z0-9-~:/?#@!$&+,;%=]+.[a-zA-Z0-9-~:/?#@!$&+,;%=]{2,}";
+    std::regex regexRule(regexPattern);
+    bool works = regex_match(url,regexRule);
+    if (works == true) 
+    {
+        url_ = url;
+        website_ = true;
+    }
+    else if(works == false)
+    {
+        url = "";
+        website_ = false;
+    }
+
+
 
 }
 
 //Mutators and Accessor Funcitons
 
-/**
-  @param  : a reference to a string representing the device
-  @post   : sets the private member variable to the value of the parameter
-**/
-//setDevice
+
 
 void Manual::setDevice (const std::string& device){
     device_=device;
 }
 
-/**
-  @return  : the variable indicating the device the manual is for
-**/
-//getDevice
-
 std::string Manual::getDevice() const{
     return device_;
 }
   
-/**
-  @param   : a reference to a website link (string) that is in the format
-             'https://www.something.something'
-             or 'http://www.something.something'
-             with the last 'something' being at least 2 characters
-  @return  : has_website_ flag (a boolean)
-  @post    : If the link is not correctly formatted,
-             store "Broken Link" in the link member variable (see <regex>)
-             and either way set the has website flag to True
-**/
-//setWebsite
-
-
 void Manual::setWebsite (const std::string& website, std::string& url) {
     std::string regexPattern = "https?://www^\\.[a-zA-Z]{2,}\\.[a-zA-Z]{2,}";
     std::regex regexRule(regexPattern);
@@ -87,35 +63,26 @@ void Manual::setWebsite (const std::string& website, std::string& url) {
     }
     else {
         url_ = "";
-        website_=false;
+        website_=false; //false before
     }
 
 }
 
+std::string Manual::getWebsite() const{
+    return url_;
+}
+
+void Manual::setVisualAid(const bool& visual_aid) {
+    visual_ = visual_aid;
+}
 
 
-/**
-  @return  : the url for the website
-**/
-// //getWebsite
+bool Manual::hasVisualAid() const{
+    return visual_;
+    return true;
+}
 
-// std::string Manual::getWebsite() const{
-//     return url_;
-// }
-
-// /**
-//   @param  : a reference to boolean
-//   @post   : sets the private member variable indicating the presence
-//             of a visual aid to the value of the parameter
-// **/
-// //setVisualAid()
-
-// void setVisualAid(const std::string &visaul_aid) {
-//     visual_=visual_aid;
-// }
-// std::string hasVisualAid() {
-//     return visual_aid;
-// }
-// std::string hasWebsite() {
-//     return website;
-// }
+bool Manual::hasWebsite() const{
+    return website_;
+    return true;
+}
---
+++ Textbook.cpp
@@ -0,0 +1,72 @@
+
+#include "Textbook.hpp"
+
+Textbook::Textbook(){
+    review_questions_ = false;
+    grade_ = NONE;
+    subject_ = "";
+
+}
+
+Textbook::Textbook(std::string title, std::string author, int page_count, std::string subject, bool review_questions, grade_level grade, bool is_digital):
+Book(title, author, page_count,is_digital){
+
+    subject_=subject;
+    grade_=grade;
+    review_questions_=review_questions;
+}
+
+
+void Textbook::setSubject (const std::string& subject) {
+    subject_=subject;
+}
+
+std::string Textbook::getSubject() const{
+    return subject_;
+}
+
+void Textbook::setGradeLevel(const grade_level& grade){
+    grade_ = grade;
+}
+
+std::string Textbook::getGradeLevel() const{
+    switch(grade_) {
+    case 0:
+    return "NONE";
+    break; 
+    case 1:
+    return "ELEMENTARY";
+    break;
+    case 2:
+    return "JUNIOR_HIGH";
+    break;
+    case 3:
+    return "HIGH_SCHOOL";
+    break;
+    case 4:
+    return "COLLEGE";
+    break;
+    }
+}
+
+void Textbook::setReviewQuestions(const bool& review_questions = true){
+    review_questions_ = review_questions;
+}
+
+/**
+  @return  : the flag indicating if the texbook has review questions
+**/
+//checkReviewQuestions
+
+bool Textbook::checkReviewQuestions() const{
+    return review_questions_;
+}
+
+
+
+
+
+
+
+
+
