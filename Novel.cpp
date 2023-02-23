be91281a822a1462e8aa8f4a0dd8679975354db1
Camillaecalle <camilla7732528@gmail.com>
2023-02-23 10:58:50 -0500

    ?

--- Novel.cpp
+++ Novel.cpp
@@ -1,9 +1,10 @@
 #include "Novel.hpp"
 
-Novel::Novel(){
+Novel::Novel() {
     genre_ = "";
-    has_film_adaptation_ = false;
-}
+    rating_={0.0};
+    has_film_adaptation_=false;
+} 
 
 //Parameterized Constructor
 
@@ -11,18 +12,16 @@ Novel::Novel(std::string title, std::string author, int page_count, std::string 
 Book(title, author, page_count, is_digital) {
     genre_=genre;
     has_film_adaptation_ = film_adaptation;
+
 }
-
-
-void Novel::setGenre(const std::string& genre) {
-    genre_ = genre;
-}
-
 
 std::string Novel::getGenre() const {
     return genre_;
 }
 
+void Novel::setGenre(const std::string& genre) {
+    genre_ = genre;
+}
 
 std::vector<char> Novel::getCharacterlist()const{ 
     return character_list_; 
@@ -38,33 +37,27 @@ std::string Novel::getCharacterListString() const{
     return result;  //NEED TO CHECKKKKKKKKKKKKKKKKKKKKKKKKKKKK
 }
 
-void Novel::addCharacter(const std::string& character_list){
+void Novel::addCharacter(const std::string& character_list) {
     character_list_.push_back(character_list);
 }
 
-
-
-bool Novel::hasFilmAdaptation() const{
+ bool Novel::hasFilmAdaptation() const{
     return has_film_adaptation_;
-}
+ }
 
 void Novel::setFilmAdaptation(const bool& has_film_adaptation){
     
-    has_film_adaptation_ = true;
-}
+     has_film_adaptation_ = true;
+ }
 
-double Novel::getAverageRating()const{ //the value of the average rating private member STILL NEED TO WRITE!!!
-
-    return 0;
-}
-
-
-std::vector<review> Novel::createReview(const double& review, std::string& rating){
+ double Novel::getAverageRating()const{ //the value of the average rating private member STILL NEED TO WRITE!!!
     
-    return reviews_;
- //   return rating_; AM I MISSING A PARAMETER???
-}
+    return average_rating_;
+ }
 
+
+
+////THIS DOWN NEEDS TO BE CHECKED
 
 
 /**
@@ -76,22 +69,34 @@ std::vector<review> Novel::createReview(const double& review, std::string& ratin
 */
 //createReview
 
+std::vector<review> Novel::createReview(const double& review, std::string& rating){
 
-void Novel::addReview(const std::vector<review> &review){ //NEED TO CHECK
-    //reviews_.insert(review);
-}
+     return reviews_.rating_;
 
-
-void Novel::calculateAverageRating(const double& average_rating){ //Need to finish!!!
-}
-
+ }
 
 /**
-  @post   : retrieves all scores from the reviews_ vector and
-            computes the average to set value of the average_rating_
-            private member
-**/  
-//calculateAverageRating
+  @param  : a reference to review object
+  @post   : inserts the review argument into the reviews_ vector
+**/
+
+
+void Novel::addReview(const std::vector<review> &review){ //NEED TO CHECK
+    reviews_.insert(review);
+}
+
+
+// void Novel::calculateAverageRating(const double& average_rating){
+//     int review;
+//     for (int i = 0; i < average_rating_.size; i++){
+//         review = review + average_rating_[i];
+//     }
+//    // average_rating_ = review/average_rating_.size();
+//     return average_rating_;
+// }
+
+
+
 
 
 
